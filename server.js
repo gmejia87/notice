96c4fc45c0530f30516e522c0467f21246631d3b
G Codes <graciela.mejia1@yahoo.com>
2022-10-08 11:38:56 -0700

    html routes complete and functioning

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -8,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
